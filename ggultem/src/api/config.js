23869f5f990cad4ca32a94dbdb911926ae23ddfb
ohjh9701 <ohjh4875@gmail.com>
2026-04-09 18:25:20 +0900

    [배포] api ssl 인증서 적용

--- ggultem/src/api/config.js
+++ ggultem/src/api/config.js
@@ -1,4 +1,4 @@
 // src/api/config.js
 //export const API_SERVER_HOST = "http://localhost:8080";
-export const API_SERVER_HOST = "http://api.ggultem.shop";
+export const API_SERVER_HOST = "https://api.ggultem.shop";
 
